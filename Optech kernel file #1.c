2b020c75f93df32680e3ff4aab0867a51f03ff92
Starcrater21 <48496899+Starcrater21@users.noreply.github.com>
2019-03-12 17:59:45 -0400

    Add files via upload

---
+++ Optech kernel file #1.c
@@ -0,0 +1,9 @@
+randomize (NULL)
+cabs = (NULL)
+if NULL = "0"
+max = "99999999999999999999999999999999999999"
+kernel
+printf (">") 
+if thread = "run OptechOS"
+installuserdriver 
+open ("Optech kernel file #2.c")
---
+++ Optech kernel file #2.c
@@ -0,0 +1,89 @@
+kernel 
+printf ("Installing optech os ")
+printf ("1%")
+installuserfont
+NULL
+acosl 
+default operator = NULL
+NULL 
+NULL
+NULL
+NULL
+NULL
+printf ("2%")
+NULL 
+NULL
+NULL
+NULL
+NULL
+printf ("3%")
+NULL 
+NULL
+NULL
+NULL
+NULL
+printf ("17%")
+NULL 
+NULL
+NULL
+NULL
+NULL
+printf ("24%")
+NULL 
+NULL
+NULL
+NULL
+NULL
+printf ("38%")
+NULL 
+NULL
+NULL
+NULL
+NULL
+printf ("53%")
+NULL 
+NULL
+NULL
+NULL
+NULL
+printf ("69%")
+NULL 
+NULL
+NULL
+NULL
+NULL
+printf ("74%")
+NULL 
+NULL
+NULL
+NULL
+NULL
+printf ("84")
+NULL 
+NULL
+NULL
+NULL
+NULL
+printf ("99%")
+NULL 
+NULL
+NULL
+NULL
+NULL
+printf ("100% Done!")
+printf ("Starting Instalization")
+NULL 
+NULL
+NULL
+NULL
+NULL
+NULL
+NULL
+NULL
+NULL
+NULL
+NULL
+NULL
+NULL
+default operator = "0"
+open ("Optech kernel file #3")
